891f9aacab6e4fa09ba1b1444378cb686946205e
Jai A Gollapudi <97879779+jaigollapudi@users.noreply.github.com>
2024-11-03 03:17:53 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for sidebar links
+document.querySelectorAll('.sidebar nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
